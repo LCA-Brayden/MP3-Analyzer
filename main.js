e2cedd40c1305055c3e73a9fa27ef458458b3ca8
Brayden-V1 <mitchellbl25@uww.edu>
2017-03-30 10:15:44 -0500

    Added Fundamental Files
    Added three files: index.html, styles.css, and main.js. Next to 0
    content within each file.

---
+++ main.js
@@ -0,0 +1,4 @@
+/* BECH MP3 Analyzer - main.js 
+	This is the main file for our mp3 analysis project. This file is meant to contain most, if not all variable declarations, and to handle user
+	input such as eventlisteners, start/stop buttons, etc. 
+Authored by Brayden Mitchell, Henry Dustrude, and Christopher Hasselquist.*/
