b1acdd2ac7bdae80d4712929d85bde1a431687ea
Brayden-V1 <mitchellbl25@uww.edu>
2017-03-30 10:48:36 -0500

    Final Base Commit
    Finishing up the basics of what is needed before branching to make more
    significant change.
    - Added paragraph of text to html explaining how the page should work.
    - Added styling to some of the html.
    
    Next goal: Create further framework, build a better base for the code to
    function. Get eventlisteners and variables in the code for use.

--- main.js
+++ main.js
@@ -2,3 +2,5 @@
 	This is the main file for our mp3 analysis project. This file is meant to contain most, if not all variable declarations, and to handle user
 	input such as eventlisteners, start/stop buttons, etc. 
 Authored by Brayden Mitchell, Henry Dustrude, and Christopher Hasselquist.*/
+
+// Variable Initialization
