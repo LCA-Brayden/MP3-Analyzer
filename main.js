36cb7d13752d56c1d318c4c24c737aa7e3e9c79f
Brayden-V1 <mitchellbl25@uww.edu>
2017-04-01 15:23:16 -0500

    Weekend Work 1 - Saturday
    - Added Scanned + .jpg copies of the plans we made on Tuesday.
    - Added empty, named buttons.
    - Added an empty <span> for filling in the current .mp3's file name.
    (for use with .innerHTML function)
    - Added some basic p5.js settings to the Setup function, and wrote up
    preload, draw, and setup functions.
    - Added event listeners to each <button> element and created empty
    functions to execute on click; created and declared variables for said
    buttons.
    - Created a slew of empty variables for future use.
    - Created a stop button to stop analysis (in theory)
    - Created a div to place the wave graph within, as a separate canvas (In
    theory), due to p5 always creating its default canvas appended.
    - Added some css styling to existing buttons.

--- main.js
+++ main.js
@@ -4,3 +4,78 @@
 Authored by Brayden Mitchell, Henry Dustrude, and Christopher Hasselquist.*/
 
 // Variable Initialization
+var startBut, stopBut, pauseBut, playBut, uploadBut, nameSpan; //HTML Elements
+var fileName; //File Name of sample/uploaded file. (String)
+var scrubber; //Current time on audio file. (float)
+var graphPos, bassLvl, lmLvl, midLvl, hmLvl, trebLvl; //Bar Graph variables (Dimensions & Height of bars)
+
+
+function preload() {
+	//Currently Empty
+}
+
+function setup() {
+	// Image/Shape Settings
+    colorMode(RGB, 255);
+    noStroke();
+    fill(200);
+    strokeCap(SQUARE);
+    strokeJoin(MITER);
+    noSmooth();
+    ellipseMode(CENTER);
+
+    // Text Display Settings
+    textAlign(CENTER, CENTER);
+    textLeading(25);
+    textSize(15);
+    textStyle(NORMAL);
+
+    // Other Settings 
+    noLoop();
+
+ //Variable Declarations
+    // Button/HTML Element initializations
+    uploadBut = document.querySelector(".upload");
+    playBut = document.querySelector(".play");
+    pauseBut = document.querySelector(".pause");
+    startBut = document.querySelector(".start");
+    stopBut = document.querySelector(".stop");
+    nameSpan = document.getElementById("fileName");
+}
+
+function draw() {
+
+
+	heyListen();
+}
+
+function heyListen() { //Adds event listeners to buttons & connects them to functions. 
+	uploadBut.addEventListener('click', uploadFile);
+	playBut.addEventListener('click', playFile);
+	pauseBut.addEventListener('click', pauseFile);
+	startBut.addEventListener('click', startAnalysis);
+	stopBut.addEventListener('click', stopAnalysis);
+}
+
+
+//Event Listener Functions
+function uploadFile() { //Prompt user for an upload, and assign the file name to a string var (for display).
+	//Called by upload button via Event Listener. 
+
+}
+
+function playFile() { //Take current file and begin play at set point. 
+
+}
+
+function pauseFile() { //Take current file and stop play at set point - make sure to save current point in scrubber.
+
+}
+
+function startAnalysis() { //Clear all current data. Begin data analysis loop.
+
+}
+
+function stopAnalysis() { //Stop current analysis loop - break with some sort of boolean (Say, an if statement within). 
+
+}
