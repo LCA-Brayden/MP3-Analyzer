83be33938f5c45e7cc70c104036dc3b7c313b76f
Brayden-V1 <mitchellbl25@uww.edu>
2017-04-08 13:28:19 -0500

    Cleanup
    removed extra files

--- MP3-Analyzer/main.js
+++ MP3-Analyzer/main.js
@@ -1,18 +1,21 @@
-/* BECH MP3 Analyzer - main.js 
+/* BECH MP3 Analyzer - main.js
 	This is the main file for our mp3 analysis project. This file is meant to contain most, if not all variable declarations, and to handle user
-	input such as eventlisteners, start/stop buttons, etc. 
+	input such as eventlisteners, start/stop buttons, etc.
 Authored by Brayden Mitchell, Henry Dustrude, and Christopher Hasselquist.*/
 
 // Variable Initialization
 var startBut, stopBut, pauseBut, playBut, uploadBut, nameSpan; //HTML Elements
 var fileName; //File Name of sample/uploaded file. (String)
 var scrubber; //Current time on audio file. (float)
+var graph = [];
+var count = 0;
 var graphPos, bassLvl, lmLvl, midLvl, hmLvl, trebLvl; //Bar Graph variables (Dimensions & Height of bars)
 var music;
 
 
 function preload() {
 	//Currently Empty
+  music = loadSound("data/GoodTimes.mp3");
 }
 
 function setup() {
@@ -31,8 +34,14 @@ function setup() {
     textSize(15);
     textStyle(NORMAL);
 
-    // Other Settings 
+    // Other Settings
     noLoop();
+    createCanvas(420,420);
+  fft = new p5.FFT();
+
+  for(var i = 0; i< 5; i++){
+    graph[i]=0;
+  }
 
  //Variable Declarations
     // Button/HTML Element initializations
@@ -42,56 +51,100 @@ function setup() {
     startBut = document.querySelector(".start");
     stopBut = document.querySelector(".stop");
     nameSpan = document.getElementById("fileName");
-	
-<<<<<<< HEAD
-    input.onchange = function(e) {
-	music = document.getElementById('music');
-	music.src = URL.createObjectURL(this.files[0]);
-	music.onend = function(e) {
-=======
-    input.onchange = function(e){
-	var sound = document.getElementById('sound');
-	sound.src = URL.createObjectURL(this.files[0]);
-	sound.onend = function(e){
->>>>>>> origin/button(sound-upload)
-		URL.revokeObjectURL(this.src);
-	}
-}
-}
 
+// <<<<<<< HEAD
+uploadBut.onchange = function() {
+ music = document.getElementById('music');
+ music.src = URL.createObjectURL(this.files[0]);
+ music = loadSound(this.files[0], playFile, null, whileLoading);
+ music.onend = function() {
+   URL.revokeObjectURL(this.src);
+ }
+}
+}
 function draw() {
 
 
 	heyListen();
 }
 
-function heyListen() { //Adds event listeners to buttons & connects them to functions. 
-	uploadBut.addEventListener('click', uploadFile);
-	playBut.addEventListener('click', playFile);
-	pauseBut.addEventListener('click', pauseFile);
-	startBut.addEventListener('click', startAnalysis);
-	stopBut.addEventListener('click', stopAnalysis);
+function heyListen() { //Adds event listeners to buttons & connects them to functions.
+	uploadBut.addEventListener("click", uploadFile);
+	playBut.addEventListener("click", playFile);
+	pauseBut.addEventListener("click", pauseFile);
+	startBut.addEventListener("click", startAnalysis);
+	stopBut.addEventListener("click", stopAnalysis);
 }
 
 
 //Event Listener Functions
 function uploadFile() { //Prompt user for an upload, and assign the file name to a string var (for display).
-	//Called by upload button via Event Listener. 
-
+	//Called by upload button via Event Listener.
 }
 
-function playFile() { //Take current file and begin play at set point. 
-
+function playFile() { //Take current file and begin play at set point.
+    // if (music.isPlaying){
+      music.play();
+    // }
 }
 
 function pauseFile() { //Take current file and stop play at set point - make sure to save current point in scrubber.
-
+    music.pause();
 }
 
 function startAnalysis() { //Clear all current data. Begin data analysis loop.
-
+  analyzeMusic();
 }
 
-function stopAnalysis() { //Stop current analysis loop - break with some sort of boolean (Say, an if statement within). 
+function stopAnalysis() { //Stop current analysis loop - break with some sort of boolean (Say, an if statement within).
+  music.pause();
+  music.currentTime = 0;
+  // music.stop();
+  clear();
+  for(var i = 0; i< 5; i++){
+    graph[i]=0;
+  }
+   fft = new p5.FFT();
+}
 
+function analyzeMusic(){//data analysis main function.
+    background(255);
+
+   fft.analyze();
+
+   noStroke();
+
+  graph[0] += fft.getEnergy("bass");
+    count += fft.getEnergy("bass");
+  graph[1] += fft.getEnergy("lowMid");
+    count += fft.getEnergy("lowMid");
+  graph[2] += fft.getEnergy("mid");
+    count += fft.getEnergy("mid");
+  graph[3] +=  fft.getEnergy("highMid");
+    count += fft.getEnergy("highMid");
+  graph[4] += fft.getEnergy("treble");
+    count += fft.getEnergy("treble");
+
+    bassMap = map(graph[0],0,count,0,420);
+    lowMidMap = map(graph[1],0,count,0,420);
+    midMap = map(graph[2],0,count,0,420);
+    highMidMap = map(graph[3],0,count,0,420);
+    trebleMap = map(graph[4],0,count,0,420);
+
+    strokeWeight(4);
+    stroke(0);
+    fill(0,255,0); // spectrum is green
+    rect(0,height,width/5,-bassMap);
+    rect(84,height,width/5,-lowMidMap);
+    rect(168,height,width/5,-midMap);
+    rect(252,height,width/5,-highMidMap);
+    rect(336,height,width/5,-trebleMap);
+
+    // stroke(255,255,255,25);
+    // fill(0,0,255,25);
+    // rect(0,height,width/5,-fft.getEnergy("bass"));
+    // rect(84,height,width/5,-fft.getEnergy("lowMid"));
+    // rect(168,height,width/5,-fft.getEnergy("mid"));
+    // rect(252,height,width/5,-fft.getEnergy("highMid"));
+    // rect(336,height,width/5,-fft.getEnergy("treble"));
 }
